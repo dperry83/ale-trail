25280ee71ab1630d7826be11b740eac3738dd09c
Ariel Sosa <sosariel98@gmail.com>
2023-04-12 22:01:59 -0500

    fundesignstuff

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -13,4 +13,8 @@
     background: rgb(6,40,81);
   background: linear-gradient(0deg, rgba(6,40,81,1) 44%, rgba(200,133,44,1) 78%);
   }
+
+  #nav {
+    text-align: end;
+  }
 </style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <router-link to="/breweries">View All Breweries</router-link> <br/>
-    <router-link to="/beers">View All Beers</router-link>
+    <div class="routers" >
+       <div style="height: 30px; width: 100%; background-color:  rgba(6,40,81,255); color: white;">
+    <router-link id="brew" to="/breweries">View All Breweries</router-link>
+    &nbsp;|&nbsp;
+    <router-link id="beer" to="/beers">View All Beers</router-link>
+    </div>
+    </div>
   </div>
 </template>
 
@@ -12,4 +17,14 @@ export default {
 };
 </script>
 
+<style scoped>
+.routers {
+ text-align: center;
+ 
+}
+h1 {
+  color: beige;
+}
+
+</style>
 
