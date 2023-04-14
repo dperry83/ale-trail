51dc871c35b024123fb124ebe52eef76da826a95
Ariel Sosa <sosariel98@gmail.com>
2023-04-14 15:42:39 -0500

    MoreFunDesignStuff

--- vue/src/views/Beers.vue
+++ vue/src/views/Beers.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="beers">
+            <the-header />
         <list-all-beers />
     </div>
 </template>
 
 <script>
 import ListAllBeers from '../components/ListAllBeers.vue'
+import TheHeader from '../components/TheHeader.vue'
 
 export default {
     name: 'AllBeers',
     components: {
-        ListAllBeers
+        ListAllBeers,
+        TheHeader
     }
 }
 </script>
