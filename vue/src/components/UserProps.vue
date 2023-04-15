372b16f27d6b33311d0c1d10a0af506161e6891d
David Perry <david.perry.83@gmail.com>
2023-04-15 17:30:47 -0400

    began working on logged-in-user functionality

---
+++ vue/src/components/UserProps.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>TEST TEXT</h1>   
+        <h2>  </h2> 
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'user-props',
+    created: {
+        
+    }
+}
+</script>
