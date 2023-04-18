a911d7dab23d5ec46cc31a1b51b38fdddb813ac5
Emmanuel Mata <emmanuelmata1278@gmail.com>
2023-04-18 14:46:06 -0500

    need reviews

---
+++ vue/src/components/GoogleMaps.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="maps">
+    <iframe
+      width="375"
+      height="350"
+      style="border:0"
+      loading="lazy"
+      allowfullscreen
+      :src="embedUrl"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      apiKey: 'AIzaSyA8sLeXelx_lS6KyS1vF0A1hD7d2DSR3Fs',
+    }
+  },
+  computed: {
+    embedUrl() {
+      return `https://www.google.com/maps/embed/v1/place?q=place_id:ChIJBRKffr8ys1IRLfE2DXkOZrQ&key=${this.apiKey}`;
+    }
+  }
+}
+</script>
