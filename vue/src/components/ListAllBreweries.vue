ce9da64bdd27b5547d58ee042be60050c4cb6a4b
David Perry <david.perry.83@gmail.com>
2023-04-19 14:51:28 -0400

    brewery sort-by-name AND router restrictions working!

--- vue/src/components/ListAllBreweries.vue
+++ vue/src/components/ListAllBreweries.vue
@@ -1,98 +1,100 @@
 <template>
-    <div id="breweryTable">
-        <table>
-            <thead>
-                <tr>
-                    <th>Name
-                        <!-- <select name="" id="sort">
-                            <option value="" v-on:click="sortByName()">Sort A-Z</option>
+  <div id="breweryTable">
+    <table>
+      <thead>
+        <tr>
+          <th>
+            Name
+            <!-- <select name="" id="sort">
+                            <option value="" v-model="">Sort A-Z</option>
                         </select> -->
-                    </th>
-                    <th>History</th>
-                    <th>Address</th>
-                    <th>Phone</th>
-                </tr>
-            </thead>
+            <input type="text" v-model="searchTerm" placeholder="search" />
+          </th>
+          <th>History</th>
+          <th>Address</th>
+          <th>Phone</th>
+        </tr>
+      </thead>
 
-            <div class="loading" v-if="isLoading">
-                <img src="beer-67.gif" />
-            </div>
-            <tbody>
-                <tr v-for="item in breweries" 
-                    v-bind:key="item.breweryId"> 
-                    <!-- links to finerdetails, using this breweryId as a parameter -->
-                    <td><router-link v-bind:to="{ name: 'finerdetails', params: {id: item.breweryId} }"> 
-                        {{ item.name }} </router-link></td>
-                    <td> {{ item.history }} </td>
-                    <td> {{ item.address }} </td>
-                    <td> {{ item.phone }} </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+      <div class="loading" v-if="isLoading">
+        <img src="beer-67.gif" />
+      </div>
+      <tbody>
+        <tr v-for="item in filteredBreweries" v-bind:key="item.breweryId">
+          <!-- links to finerdetails, using this breweryId as a parameter -->
+          <td>
+            <router-link
+              v-bind:to="{
+                name: 'finerdetails',
+                params: { id: item.breweryId },
+              }"
+            >
+              {{ item.name }}
+            </router-link>
+          </td>
+          <td>{{ item.history }}</td>
+          <td>{{ item.address }}</td>
+          <td>{{ item.phone }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import BreweryService from '../services/BreweryService';
+import BreweryService from "../services/BreweryService";
 
 export default {
-    name: 'brewery-list',
-    data() {
-        return {
-            breweries: [],
-            isLoading: true,
-        }
+  name: "brewery-list",
+  data() {
+    return {
+      breweries: [],
+      isLoading: true,
+      searchTerm: "",
+    };
+  },
+  // calls BreweryService and populates data {breweries} w/ info at that endpoint
+  created() {
+    BreweryService.getAllBreweries().then((response) => {
+      this.breweries = response.data;
+      this.isLoading = false;
+    });
+  },
+  computed: {
+    filteredBreweries() {
+      return this.breweries.filter((item) => {
+        return item.name.toLowerCase().includes(this.searchTerm.toLowerCase());
+      });
     },
-    // calls BreweryService and populates data {breweries} w/ info at that endpoint
-    created() {
-        BreweryService
-            .getAllBreweries()
-            .then( response => {
-                this.breweries = response.data;
-                this.isLoading = false;
-            })
+  },
+  methods: {
+    sortByName() {
+      this.breweries.sort((a, b) => {
+        return a.name - b.name;
+      });
     },
-    methods: {
-        sortByName() {
-            this.breweries.sort((a, b) => {
-                const nameA = a.name;
-                const nameB = b.name;
-                if (nameA < nameB) {
-                    return -1;
-                }
-                if (nameA > nameB) {
-                    return 1;
-                }
-                return 0;
-            })
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 table {
-    width: 80%;
-    margin-left: 10%;
-    border-collapse: collapse;
-    
+  width: 80%;
+  margin-left: 10%;
+  border-collapse: collapse;
 }
-th, td {
-    text-align: center; 
-    border: 1px solid black;
-    padding: 13px 10px;
+th,
+td {
+  text-align: center;
+  border: 1px solid black;
+  padding: 13px 10px;
 }
 td {
-    padding-right: 10px;
-    vertical-align: top;
+  padding-right: 10px;
+  vertical-align: top;
 }
 
-div#breweryTable{
-   padding-top: 30px;
+div#breweryTable {
+  padding-top: 30px;
 }
-
-
-
-
-
 </style>
--- vue/src/views/Breweries.vue
+++ vue/src/views/Breweries.vue
@@ -2,19 +2,22 @@
     <div class="breweries">
         <the-header />
     <list-all-breweries />
+    <the-footer />
     </div>
 </template>
 
 <script>
 import ListAllBreweries from '../components/ListAllBreweries.vue'
 import TheHeader from '../components/TheHeader.vue'
+import TheFooter from '../components/TheFooter.vue';
 
 export default {
     // name given to ListAllBreweries component
     name: 'AllBreweries',
     components: {
         ListAllBreweries,
-        TheHeader
+        TheHeader,
+        TheFooter
     }
 }
 </script>
